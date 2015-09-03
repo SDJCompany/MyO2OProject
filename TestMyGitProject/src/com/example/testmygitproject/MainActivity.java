7b817c6eb89f080db79263436832504dd378ddee
JerryShin <m9511003@gmail.com>
2015-09-03 13:59:25 +0900

    내용 수정

--- TestMyGitProject/src/com/example/testmygitproject/MainActivity.java
+++ TestMyGitProject/src/com/example/testmygitproject/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		Button callB = (Button) findViewById(R.id.callBut);
+		
+		//======== 동진이가 수정했어요 : 123456
+		
+		
 	}
 
 	
